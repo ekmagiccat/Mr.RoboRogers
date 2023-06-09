5f54b59624cbc71ebd4d81f425d53eec03801f9d
Eva Kemp <eva.j.kemp@gmail.com>
2023-06-09 14:13:40 -0700

    create base html page

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
 function mrRogers(text) {
   let num1 = ["1"];
+  let num2 = ["2"];
+  const special = /\b\d*1\d*\b/g;
+  let numTotal = text;
+  num1.forEach(function (word) {
+    numTotal = numTotal.replaceAll(special, "Beep!");
+  });
+    num2.forEach(function (word2) {
+      numTotal = numTotal.replaceAll(special, "Boop!");
+    });
+  });
+  return numTotal;
+}
+
+/*
+function mrRogers(text) {
+  let num = ["1"];
   const special = /\b\d*1\d*\b/g;
   let num1Total = text;
   num1.forEach(function (word) {
