f09007d956ab81df5b8644e8a179fa3fba724f04
Eva Kemp <eva.j.kemp@gmail.com>
2023-06-09 15:11:41 -0700

    wip

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,7 @@
+// code that works for test 1:
+
 function mrRogers(text) {
   let num1 = ["1"];
-  let num2 = ["2"];
-  const special = /\b\d*1\d*\b/g;
-  let numTotal = text;
-  num1.forEach(function (word) {
-    numTotal = numTotal.replaceAll(special, "Beep!");
-  });
-    num2.forEach(function (word2) {
-      numTotal = numTotal.replaceAll(special, "Boop!");
-    });
-  });
-  return numTotal;
-}
-
-/*
-function mrRogers(text) {
-  let num = ["1"];
   const special = /\b\d*1\d*\b/g;
   let num1Total = text;
   num1.forEach(function (word) {
@@ -23,3 +9,23 @@ function mrRogers(text) {
   });
   return num1Total;
 }
+
+/* code that doesn't work
+function mrRogers(text) {
+  let num1 = ["1"];
+  let num2 = ["2"];
+  let num3 = ["3"];
+  const special = /\b\d*1\d*\b/g;
+  let numTotal = [text];
+  numTotal.forEach(function (word) {
+    if (numTotal.includes(num1)) {
+        result = numTotal.replaceAll(special, "Beep!"); 
+    } else if (text.includes(num2)) {
+        result = numTotal.replaceAll(special, "Boop!");
+    } else {
+
+    }
+  });
+  return numTotal;
+}
+*/
