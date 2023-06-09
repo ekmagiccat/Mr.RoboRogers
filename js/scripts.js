fdccee6f71364a8b43275c7890ffe2f229ccbcda
Eva Kemp <eva.j.kemp@gmail.com>
2023-06-09 13:34:57 -0700

    fix first test to replace all digits containing 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 function mrRogers(text) {
   let num1 = ["1"];
+  const special = /\b\d*1\d*\b/g;
   let num1Total = text;
   num1.forEach(function (word) {
-    num1Total = num1Total.replace(word, "Beep!");
+    num1Total = num1Total.replaceAll(special, "Beep!");
   });
   return num1Total;
 }
